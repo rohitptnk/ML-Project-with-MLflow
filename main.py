f35effbd03f0a230aee241a67b6642d6c7baf6d3
Rohit Patnaik <rohitptnk03@gmail.com>
2025-11-27 16:35:15 +0530

    Add data validation pipeline

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from mlFlowProject import logger
 from mlFlowProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from mlFlowProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
